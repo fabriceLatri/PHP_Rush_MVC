b29e1eb8bd8178a2006baed6cb1daa864ccc9707
maxim3andr3 <maxim3andr3@gmail.com>
2019-07-31 14:07:39 +0200

    Modified routes.php : adding the path to the true_registration_page

--- config/routes.php
+++ config/routes.php
@@ -3,3 +3,5 @@
 $router->use('GET', '/auth/register', new App\Controllers\AuthController(), 'register_view');
 $router->use('POST', '/auth/register', new App\Controllers\AuthController(), 'register');
 $router->use('GET', '/error404', new App\Controllers\Error404Controller(), 'error404_view');
+$router->use('GET', '/register/true_register', new App\Controllers\RegisterController(), 'true_register_view');
+$router->use('POST', '/register/true_register', new App\Controllers\RegisterController(), 'true_register');
