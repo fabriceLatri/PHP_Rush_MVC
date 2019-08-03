4810cac56ae4da61fa4269d7792ae8dac3094e02
fabolododo <fabolododo@hotmail.fr>
2019-08-03 12:37:03 +0200

    modified routes

--- config/routes.php
+++ config/routes.php
@@ -8,6 +8,9 @@ $router->use('GET', '/error404', new App\Controllers\Error404Controller(), 'erro
 $router->use('GET', '/auth/login', new App\Controllers\LoginController(), 'login_view');
 $router->use('POST', '/auth/login', new App\Controllers\LoginController(), 'login');
 
+$router->use('GET', '/auth/logout', new App\Controllers\LogoutController(), 'logout_view');
+$router->use('POST', '/auth/logout', new App\Controllers\LogoutController(), 'logout');
+
 $router->use('GET', '/users/userPanel', new App\Controllers\UserPanelController(), 'user_view');
 $router->use('POST', '/users/userPanel', new App\Controllers\UserPanelController(), 'user');
 $router->use('GET', '/articles/addArticle', new App\Controllers\ArticleController(), 'addArticle_view');
