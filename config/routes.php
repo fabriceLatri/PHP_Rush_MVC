5fb087c39a61a6b2b2c31f9343ad387ffe827c1e
fabolododo <fabolododo@hotmail.fr>
2019-08-04 18:44:08 +0200

    modified routes.php

--- config/routes.php
+++ config/routes.php
@@ -13,7 +13,8 @@ $router->use('POST', '/auth/logout', new App\Controllers\LogoutController(), 'lo
 
 $router->use('GET', '/users/userPanel', new App\Controllers\UserPanelController(), 'user_view');
 $router->use('POST', '/users/userPanel', new App\Controllers\UserPanelController(), 'user');
+
 $router->use('GET', '/articles/addArticle', new App\Controllers\ArticleController(), 'addArticle_view');
 $router->use('POST', '/articles/addArticle', new App\Controllers\ArticleController(), 'addArticle');
-
 $router->use('GET', '/articles/listArticle', new App\Controllers\ListarticleController(), 'listArticle_view');
+
