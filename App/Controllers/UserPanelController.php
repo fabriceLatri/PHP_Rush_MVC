37a163bc03d49652fb9665331c88a3ce678e00c3
maxim3andr3 <maxim3andr3@gmail.com>
2019-08-05 06:58:01 +0200

    Modified UserPanelController.php : adding a statement to redirect if not connected

--- App/Controllers/UserPanelController.php
+++ App/Controllers/UserPanelController.php
@@ -13,42 +13,46 @@ class UserPanelController extends AppController
 {
   public function user_view(Request $request)
   {
-    $user = new user();
-    $user->setId($_SESSION['id']);
-    $userInfo = $this->orm->prepareRequest($user, "selectUserId", ['id']);
-    return $this->render('users/userPanel.html.twig', ['base' => $request->base,
-      'error' => $this->flashError,
-      'userInfo' => $userInfo]);
-  }
-
-  public function user(Request $request) { 
-    $user = new User();
-    $user->setUsername($request->params['username']);
-    $user->setEmail($request->params['email']);
-
-    try {
-      $user->validate();
-    } catch (\Exception $e) {
-      $this->flashError->set($e->getMessage());
-      $this->redirect('/' . $request->base . 'users/userPanel', '302');
-      return;
+      if (!empty($_SESSION['id'])) {
+      $user = new user();
+      $user->setId($_SESSION['id']);
+      $userInfo = $this->orm->prepareRequest($user, "selectUserId", ['id']);
+      return $this->render('users/userPanel.html.twig', ['base' => $request->base,
+        'error' => $this->flashError,
+        'userInfo' => $userInfo]);
+      } else {
+        header('location:/PHP_Rush_MVC/auth/login');
+      }
     }
-
-    $query = $this->orm->getDb()->prepare($user->addUser());
-    $array = [
-      'username' => $request->params['username'],
-      'email' => $request->params['email'],
-      'user_group' => $request->params['user_group'],
-    ];
-
-    $query->execute($array);
-
-    header('location:/PHP_Rush_MVC/users/userPanel');
-
-    // return $this->render('users/userPanel.html.twig', ['base' => $request->base,
-    // 'error' => $this->flashError,
-    // 'data' => $array]);
-
-    die();
-  }
+  
+    public function user(Request $request) { 
+      $user = new User();
+      $user->setUsername($request->params['username']);
+      $user->setEmail($request->params['email']);
+  
+      try {
+        $user->validate();
+      } catch (\Exception $e) {
+        $this->flashError->set($e->getMessage());
+        $this->redirect('/' . $request->base . 'users/userPanel', '302');
+        return;
+      }
+  
+      $query = $this->orm->getDb()->prepare($user->addUser());
+      $array = [
+        'username' => $request->params['username'],
+        'email' => $request->params['email'],
+        'user_group' => $request->params['user_group'],
+      ];
+  
+      $query->execute($array);
+  
+      header('location:/PHP_Rush_MVC/users/userPanel');
+  
+      // return $this->render('users/userPanel.html.twig', ['base' => $request->base,
+      // 'error' => $this->flashError,
+      // 'data' => $array]);
+  
+      die();
+    }
 }
